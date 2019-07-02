a049b9db413399bc3a076ba2f44488c26d376d8b
kabuhakmeh3 <kabuhakmeh@gmail.com>
2019-07-01 20:28:23 -0700

    functionalized script to run remotely

--- read_tables.py
+++ read_tables.py
@@ -1,9 +1,11 @@
+import os, pickle
 from sqlalchemy import create_engine
 from connect_mysql import auth_db
 import pandas as pd
-import pickle
 
-# dynmically build sql query to only choose specific columns
+# Suggestions for improvement
+#
+# build sql query to only choose specific columns
 #
 # referrals
 #   club, first_name, last_name, email, mobile, campaign
@@ -17,23 +19,29 @@ def list_to_pickle(file_to_write, list_to_write):
     with open(file_to_write, 'wb') as fp:
         pickle.dump(list_to_write, fp)
 
-engine = create_engine(auth_db())
 
-sql_refr = "SELECT email, mobile from referrals"
-sql_lead = "SELECT email, mobile_phone as mobile from leads"
+def main(list_path='./sample_data'):
+    
+    # load credentials & create engine
+    engine = create_engine(auth_db())
+    
+    # define sql queries for each table
+    sql_refr = "SELECT email, mobile from referrals"
+    sql_lead = "SELECT email, mobile_phone as mobile from leads"
 
-df_refr = pd.read_sql(sql_refr, engine, index_col=None)
-df_lead = pd.read_sql(sql_lead, engine, index_col=None)
+    # read & merge lead data
+    df_refr = pd.read_sql(sql_refr, engine, index_col=None)
+    df_lead = pd.read_sql(sql_lead, engine, index_col=None)
 
-df = pd.concat([df_refr, df_lead])
+    df = pd.concat([df_refr, df_lead])
 
-print('Read {} records from database'.format(df.shape[0]))
+    # create lists of lead contact info
+    list_email = df['email'].dropna().tolist()
+    list_phone = df['mobile'].dropna().tolist()
 
-list_email = df['email'].dropna().tolist()
-list_phone = df['mobile'].dropna().tolist()
+    # write lists to file
+    list_to_pickle(os.path.join(list_path, 'emails.pckl'), list_email)
+    list_to_pickle(os.path.join(list_path, 'phones.pckl'), list_phone)
 
-print('{} emails and {} phone numbers read'.format(
-    len(list_email), len(list_phone)))
-
-list_to_pickle('./sample_data/emails.pckl', list_email)
-list_to_pickle('./sample_data/phones.pckl', list_phone)
+if __name__ == '__main__':
+    main()
