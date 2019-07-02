2416cf0ca9c05412bafaf7d767d2ca5afe913f71
kabuhakmeh3 <kabuhakmeh@gmail.com>
2019-07-01 19:32:04 -0700

    updated sql select query

--- read_tables.py
+++ read_tables.py
@@ -2,8 +2,25 @@ from sqlalchemy import create_engine
 from connect_mysql import auth_db
 import pandas as pd
 
+# dynmically build sql query to only choose specific columns
+#
+# referrals
+#   club, first_name, last_name, email, mobile, campaign
+#
+# leads
+#   first_name, last_name, email, mobile_phone
+#
+# Note: consider only importing phone & email
+
 engine = create_engine(auth_db())
 
-sql = "SELECT * from referrals WHERE campaign='buddy'"
+sql_refr = "SELECT email, mobile from referrals"
 
-df = pd.read_sql(sql, engine, index_col=None)
+sql_lead = "SELECT email, mobile_phone as mobile from leads"
+
+df_refr = pd.read_sql(sql_refr, engine, index_col=None)
+df_lead = pd.read_sql(sql_lead, engine, index_col=None)
+
+df = pd.concat([df_refr, df_lead])
+
+print(df.head())
