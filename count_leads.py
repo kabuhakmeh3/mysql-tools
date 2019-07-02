b9b22b1dc1dba49f1460fa3a6fd83fb343dc7ed1
kabuhakmeh3 <kabuhakmeh@gmail.com>
2019-07-02 13:45:14 -0700

    added date query and lead counts

--- count_leads.py
+++ count_leads.py
@@ -1,4 +1,4 @@
-import os, pickle
+import os, datetime, pickle
 from sqlalchemy import create_engine
 from connect_mysql import auth_db
 import pandas as pd
@@ -8,38 +8,46 @@ import pandas as pd
 # build sql query to only choose specific columns
 #
 # referrals
-#   club, first_name, last_name, email, mobile, campaign
+#   club, first_name, last_name, email, mobile, campaign, created_at
 #
 # leads
-#   first_name, last_name, email, mobile_phone
+#   first_name, last_name, email, mobile_phone, created_at
 #
 # Note: consider only importing phone & email
 
+# results will feed into dashboard
+# consider writing/appending to csv instead
 def list_to_pickle(file_to_write, list_to_write):
     with open(file_to_write, 'wb') as fp:
         pickle.dump(list_to_write, fp)
 
 
+def get_date_range(timespan=1):
+    '''Build a date range for queries
+    '''
+    now=f"{datetime.datetime.now():%Y-%m-%d}"
+    start=f"{datetime.datetime.now()-datetime.timedelta(timespan):%Y-%m-%d}"
+    date_range = "'" + start + "'" + " and " + "'" + now + "'"
+
+    return date_range
+
+#def build_query():
+#    return query
+
 def main(list_path='./sample_data'):
-    
+
     # load credentials & create engine
     engine = create_engine(auth_db())
-    
-    # define sql queries for each table
-    date_query = "BETWEEN '2019-07-01' and '2019-07-02'"
-    sql_refr = "SELECT email, mobile from referrals WHERE created_at " + date_query
-    sql_lead = "SELECT email, mobile_phone as mobile from leads WHERE created_at " + date_query
 
-    # read & merge lead data
-    df_refr = pd.read_sql(sql_refr, engine, index_col=None)
-    df_lead = pd.read_sql(sql_lead, engine, index_col=None)
+    sql = "SELECT campaign, COUNT(campaign) as leads " +\
+    "from referrals " +\
+    "WHERE created_at " +\
+    "BETWEEN " + get_date_range() +\
+    " GROUP BY campaign"
 
-    df = pd.concat([df_refr, df_lead])
+    df = pd.read_sql(sql, engine, index_col=None)
 
-    # test_output
-    print(df.head())
-    print(df.tail())
-    print('{} leads since ...'.format(df.shape[0]))
+    print(df)
 
 if __name__ == '__main__':
     main()
