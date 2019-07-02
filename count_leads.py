cf56685069f1a1c32051a9277dcd6031d17c39f7
kabuhakmeh3 <kabuhakmeh@gmail.com>
2019-07-02 15:23:19 -0700

    write results to csv

--- count_leads.py
+++ count_leads.py
@@ -1,25 +1,16 @@
 import os, datetime, pickle
+import pandas as pd
 from sqlalchemy import create_engine
 from connect_mysql import auth_db
-import pandas as pd
 
-# Suggestions for improvement
-#
-# build sql query to only choose specific columns
+
+# Database relevant columns
 #
 # referrals
-#   club, first_name, last_name, email, mobile, campaign, created_at
+#   campaign, created_at
 #
 # leads
-#   first_name, last_name, email, mobile_phone, created_at
-#
-# Note: consider only importing phone & email
-
-# results will feed into dashboard
-# consider writing/appending to csv instead
-def list_to_pickle(file_to_write, list_to_write):
-    with open(file_to_write, 'wb') as fp:
-        pickle.dump(list_to_write, fp)
+#   created_at
 
 
 def get_date_range(timespan=1):
@@ -31,23 +22,57 @@ def get_date_range(timespan=1):
 
     return date_range
 
-#def build_query():
-#    return query
-
-def main(list_path='./sample_data'):
-
-    # load credentials & create engine
-    engine = create_engine(auth_db())
-
+def query_referrals(dates, engine):
+    '''Generate SQL query & return lead counts grouped by campaign
+    '''
     sql = "SELECT campaign, COUNT(campaign) as leads " +\
     "from referrals " +\
     "WHERE created_at " +\
-    "BETWEEN " + get_date_range() +\
+    "BETWEEN " + dates +\
     " GROUP BY campaign"
 
     df = pd.read_sql(sql, engine, index_col=None)
 
-    print(df)
+    return df
+
+def query_leads(dates, engine):
+    '''Generate SQL query & process df to match referral format
+    '''
+
+    sql= "SELECT COUNT(created_at) as leads " +\
+    "from leads " +\
+    "WHERE created_at " +\
+    "BETWEEN " + dates
+
+    df = pd.read_sql(sql, engine, index_col=None)
+    df['campaign'] = 'payment'
+    df = df[['campaign', 'leads']]
+
+    return df
+
+def main(target_path='./sample_data'):
+
+    # load credentials & create engine
+    db_engine = create_engine(auth_db())
+
+    # get correct dates
+    now = datetime.datetime.now()-datetime.timedelta(1)
+    date_range = get_date_range(timespan=1)
+
+    # query referral database
+    df_ref = query_referrals(dates=date_range, engine=db_engine)
+
+    # query lead database
+    df_lead = query_leads(dates=date_range, engine=db_engine)
+
+    # combine query results
+    df = pd.concat([df_ref, df_lead])
+    df['date'] = f"{now:%Y-%m-%d}"
+    df = df[['date','campaign','leads']]
+
+    # write output to file
+    target_file = os.path.join(target_path, 'lead_totals.csv')
+    df.to_csv(target_file, mode='a', header=False, index=False)
 
 if __name__ == '__main__':
     main()
